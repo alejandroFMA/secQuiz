eb909035855136c709fc59e3ed662c5908951d5f
Cristina Mateo <cris.mateo.99@gmail.com>
2023-10-20 20:20:35 +0200

    se pinta solo 1 en pantalla

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//firebase
+
+
+
+//adventlist de boton de inicio con llamada a la funcion
+
+
+
+
+
 const api = 'https://opentdb.com/api.php?amount=10&category=14&difficulty=medium&type=multiple' 
 
 const preguntas = [] 
@@ -9,9 +19,41 @@ let numbers = [0,1,2,3]
 function caos(array) {
     array.sort(() => Math.random() - 0.5);
     return array
-    
   }
- 
+
+function pintar(pregunta, mezcladas, i){
+    let rnd = caos(numbers)
+    let template = document.getElementById("section") 
+    
+     template.innerHTML = 
+    `<fieldset class="field hide" id="${[i]}">
+        <legend>${pregunta}</legend>
+        <div>
+            <label for="a${i}">${mezcladas[rnd[0]]}</label>
+            <input type="radio" name="${pregunta}" value="${mezcladas[rnd[0]]}" id= "a${i}"  required>
+        </div>
+        <div>
+            <label for="b${i}">${mezcladas[rnd[1]]}</label>
+            <input type="radio" name="${pregunta}" value="${mezcladas[rnd[1]]}" id= "b${i}"  required>
+        </div>
+        <div>
+            <label for="c${i}">${mezcladas[rnd[2]]}</label>
+            <input type="radio" name="${pregunta}" value="${mezcladas[rnd[2]]}" id= "c${i}"  required>
+        </div>
+        <div>
+            <label for="d${i}">${mezcladas[rnd[3]]}</label>
+            <input type="radio" name="${pregunta}" value="${mezcladas[rnd[3]]}" id= "d${i}"  required>
+        </div>
+    </fieldset>
+    <button id="next">NEXT</button>
+    ` 
+
+    document.querySelector("#next").addEventListener("click", comprobar)
+}
+
+
+
+ //Crear form
 async function getQuiz() {
 
     let response = await fetch(api);
@@ -20,53 +62,51 @@ async function getQuiz() {
     for(let i=0; i< data.results.length; i++){     
       
         preguntas.push(data.results[i].question)                         
-    
-       correctas.push(data.results[i].correct_answer)
-       
-
+        correctas.push(data.results[i].correct_answer)
         mezcladas.push(data.results[i].incorrect_answers.concat(data.results[i].correct_answer))    
         
     }
-
-    console.log(mezcladas)
-
-
-    for (let i = 0; i < preguntas.length; i++) {
-
-        let rnd = caos(numbers)
-    
-        let template = document.getElementById("form") 
-        
-         template.innerHTML += 
-        `<fieldset>
-            <legend>${preguntas[i]}</legend>
-            <div>
-                <label for="${mezcladas[i][rnd[0]]}">${mezcladas[i][rnd[0]]}</label>
-                <input type="radio" name="${mezcladas[i][rnd[0]]}" value="${mezcladas[i][rnd[0]]}">
-            </div>
-            <div>
-                <label for="${mezcladas[i][rnd[1]]}">${mezcladas[i][rnd[1]]}</label>
-                <input type="radio" name="${mezcladas[i][rnd[1]]}" value="${mezcladas[i][rnd[1]]}">
-            </div>
-            <div>
-                <label for="${mezcladas[i][rnd[2]]}">${mezcladas[i][rnd[2]]}</label>
-                <input type="radio" name="${mezcladas[i][rnd[2]]}" value="${mezcladas[i][rnd[2]]}">
-            </div>
-            <div>
-                <label for="${mezcladas[i][rnd[3]]}">${mezcladas[i][rnd[3]]}</label>
-                <input type="radio" name="${mezcladas[i][rnd[3]]}" value="${mezcladas[i][rnd[3]]}">
-            </div>
-        </fieldset>
-        ` 
- 
+    let i = 0
+      pintar(preguntas[i], mezcladas[i], i)  
     }
     
+getQuiz();
+
+//validación
+
+function comprobar(event){
+    event.preventDefault();
+    console.log(event)
+   
+console.log("Holaaa")
+    const respuestaUsuario = document.querySelectorAll("input:checked").nodeValue
+    let alerta ="";
+    let score = 0
+
+    for (let i = 0; i < preguntas.length; i++) {
+       
+        if(respuestaUsuario[i] != correctas[i]){
+            alerta+="Respuesta incorrecta!";
+        } else if(respuestaUsuario[i].nodeValue == correctas[i]){
+            score++
+        }  
+    }
+
+    console.log(alerta)
+    console.log(score)
 }
 
 
-getQuiz();
+document.querySelector("#form").addEventListener("submit", comprobar)
 
 
-
+//pintarUsuarios(usuarios);
+//if (!boolean) {
+ //   Swal.fire({
+   //     icon: 'error',
+    //    title: 'Vaya...',
+     //   text: 'No hay ningún usuario con ese nombre',
+    //  });
+//}
 
 
